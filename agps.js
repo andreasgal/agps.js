4c622e20e32a2205784d62058305bc35bfa794d9
Andreas Gal <andreas.gal@gmail.com>
2015-02-14 15:31:48 -0800

    fetch data on a timer, not on every request

--- agps.js
+++ agps.js
@@ -19,37 +19,56 @@ function fetchAndParse(source, callback) {
   });
 }
 
+// Update our AGPS data every 5 minutes.
+var state = {
+  data: null,
+  timestamp: null,
+};
+function update() {
+  fetchAndParse(source, function (err, data) {
+    if (err)
+      return;
+    state.data = data;
+    state.timestamp = Date.now();
+  });
+  setTimeout(update, 5 * 60 * 1000);
+}
+update();
+
 var app = express();
 app.use(compress());
 
 app.get('/', function (req, res) {
-  fetchAndParse(source, function (err, data) {
-    if (err) {
-      res.status(500).send("Internal error");
-      return;
-    }
-    res.setHeader('Cache-Control', 'public, max-age=300');
-    res.format({
-      'application/json': function() {
-        res.send(data);
-      },
-      'text/html': function() {
-        var html = "<html>";
-        html += "<head><title>AGPS status</title></head>";
-        html += "<body>";
-        for (var n = 1; n <= 32; ++n) {
-          html += "<table>";
-          var eph = data[n];
-          Object.keys(eph).forEach(function (key) {
-            html += "<tr><td><b>" + key + "</b>: </td><td>" + eph[key] + "</td></td>";
-          });
-          html += "</table>";
-        }
-        html += "</body>";
-        html += "</html>";
-        res.send(html);
+  if (!state.data) {
+    res.status(503).send("Service Unavailable");
+    return;
+  }
+  // We update every 5 minutes and allow 5 minutes of caching. A client should never
+  // receive data that is more than 10 minutes old as far as this server is concerned.
+  // The source data might be up to an hour old. Ephemeris data is valid for 4 hours
+  // so we should be well within the validity period.
+  res.setHeader('Cache-Control', 'public, max-age=300');
+  res.format({
+    'application/json': function() {
+      res.send(state.data);
+    },
+    'text/html': function() {
+      var html = "<html>";
+      html += "<head><title>AGPS status</title></head>";
+      html += "<body>";
+      html += "<h1>Last updated: " + new Date(state.timestamp) + "</h1>";
+      for (var n = 1; n <= 32; ++n) {
+        html += "<table>";
+        var eph = state.data[n];
+        Object.keys(eph).forEach(function (key) {
+          html += "<tr><td><b>" + key + "</b>: </td><td>" + eph[key] + "</td></td>";
+        });
+        html += "</table>";
       }
-    });
+      html += "</body>";
+      html += "</html>";
+      res.send(html);
+    }
   });
 });
 
